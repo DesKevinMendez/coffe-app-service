98a71889b0008e05517db3147f53645bcf68c60d
Kevin <kevin_mendez30@outlook.com>
2022-09-25 19:11:28 -0600

    feat: 🎸 (vue) added vue 3 with vite

---
+++ resources/js/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { RouterView } from "vue-router";
+// import { useMainStore } from "@/stores/main.js";
+
+// const mainStore = useMainStore();
+
+// mainStore.setUser({
+//     name: "John Doe",
+//     email: "john@example.com",
+//     avatar: "https://avatars.dicebear.com/api/avataaars/example.svg?options[top][]=shortHair&options[accessoriesChance]=93",
+// });
+</script>
+
+<template>
+    <h1>Vue app</h1>
+    <!-- <RouterView /> -->
+</template>
