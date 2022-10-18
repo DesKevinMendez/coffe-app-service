ed3d2ce548608ed3967e2a614258b662572e162f
Kevin <kevin_mendez30@outlook.com>
2022-10-17 22:29:01 -0500

    refactor: 💡 (backend) routing group

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\Api\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [LoginController::class, 'login'])->name('api.v1.login')
-->middleware('guest:sanctum');
+Route::name('api.v1.')->group(function () {
+    Route::post('login', [LoginController::class, 'login'])
+        ->name('login')
+        ->middleware('guest:sanctum');
+});
