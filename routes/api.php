b198a0d1c653832dfb3df2a43838a3360ae8f30a
Kevin <kevin_mendez30@outlook.com>
2022-11-09 09:42:57 -0600

    test: 💍 (Backend) route for register

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\API\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.v1.')->group(function () {
     Route::post('login', [LoginController::class, 'login'])
         ->name('login')
         ->middleware('guest:sanctum');
+
+    Route::post('register', [RegisterController::class, 'register'])
+        ->middleware('guest:sanctum')
+        ->name('register');
 });
