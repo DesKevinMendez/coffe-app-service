d049b8bd8631dbe7aba07e28e2a4977c896d8410
Kevin <kevin_mendez30@outlook.com>
2022-11-09 09:22:06 -0600

    feat: 🎸 (Register) view

--- resources/js/src/components/template/SectionFormScreen.vue
+++ resources/js/src/components/template/SectionFormScreen.vue
@@ -7,42 +7,52 @@ import {
   sectionBgLoginDark,
   sectionBgError,
   sectionBgErrorDark,
+  sectionBgSignupDark,
+  sectionBgSignup,
+  sectionBgRemind,
+  sectionBgRemindDark,
 } from '@/core/colors';
 import KevinLogo from '@/components/template/KevinLogo.vue';
 import BaseButtons from '@/components/template/BaseButtons.vue';
 import BaseButton from '@/components/template/BaseButton.vue';
 
+
 const props = defineProps({
   bg: {
     type: String,
     required: true,
-    validator: (value) =>
-      ['login', 'signup', 'remind', 'error'].includes(value),
+    validator: value => ['login', 'signup', 'remind', 'error'].includes(value)
   },
-  promo: Boolean,
-});
+  promo: Boolean
+})
 
-const darkMode = computed(() => useStyleStore().darkMode);
+const darkMode = computed(() => useStyleStore().darkMode)
 
 const colorClass = computed(() => {
   switch (props.bg) {
   case 'login':
-    return darkMode.value ? sectionBgLoginDark : sectionBgLogin;
+    return darkMode.value ? sectionBgLoginDark : sectionBgLogin
   case 'error':
-    return darkMode.value ? sectionBgErrorDark : sectionBgError;
+    return darkMode.value ? sectionBgErrorDark : sectionBgError
+  case 'signup':
+    return darkMode.value ? sectionBgSignupDark : sectionBgSignup
+  case 'remind':
+    return darkMode.value ? sectionBgRemindDark : sectionBgRemind
   }
 
-  return '';
-});
+  return ''
+})
 
-const route = useRoute();
+const route = useRoute()
 
-const currentRouteName = route.name;
+const currentRouteName = route.name
 
 const routes = {
   login: 'Login',
-  error: 'Error',
-};
+  signup: 'Signup',
+  // remind: 'Remind',
+  error: 'Error'
+}
 </script>
 
 <template>
@@ -50,9 +60,15 @@ const routes = {
     class="section-screen-content transition-background-image"
     :class="colorClass"
   >
-    <div v-if="promo" class="space-y-12 px-12">
+    <div
+      v-if="promo"
+      class="space-y-12 px-12"
+    >
       <div class="hidden lg:block">
-        <BaseButtons type="justify-center" glue>
+        <BaseButtons
+          type="justify-center"
+          glue
+        >
           <BaseButton
             v-for="(routeLabel, index) in routes"
             :key="index"
@@ -68,15 +84,23 @@ const routes = {
       </div>
 
       <div class="text-center text-white py-12 md:py-0">
-        <h1 class="text-5xl lg:text-6xl font-black">All instances</h1>
-        <h2 class="text-2xl">managed from one place</h2>
+        <h1 class="text-5xl lg:text-6xl font-black">
+          All instances
+        </h1>
+        <h2 class="text-2xl">
+          managed from one place
+        </h2>
       </div>
-      <div
-        class="hidden md:block py-12 md:py-0 text-center text-white text-opacity-50 dark:text-opacity-80"
-      >
-        <RouterLink to="/"> Instagram </RouterLink> |
-        <RouterLink to="/"> Telegram </RouterLink> |
-        <RouterLink to="/"> Teletype </RouterLink>
+      <div class="hidden md:block py-12 md:py-0 text-center text-white text-opacity-50 dark:text-opacity-80">
+        <RouterLink to="/">
+          Instagram
+        </RouterLink> |
+        <RouterLink to="/">
+          Telegram
+        </RouterLink> |
+        <RouterLink to="/">
+          Teletype
+        </RouterLink>
       </div>
       <div class="hidden md:block text-white">
         <RouterLink to="/">
@@ -90,11 +114,20 @@ const routes = {
       card-rounded="rounded-lg"
     />
 
-    <div v-if="promo" class="md:hidden space-y-12 py-12">
+    <div
+      v-if="promo"
+      class="md:hidden space-y-12 py-12"
+    >
       <div class="text-white text-opacity-50">
-        <RouterLink to="/"> Instagram </RouterLink> |
-        <RouterLink to="/"> Telegram </RouterLink> |
-        <RouterLink to="/"> Teletype </RouterLink>
+        <RouterLink to="/">
+          Instagram
+        </RouterLink> |
+        <RouterLink to="/">
+          Telegram
+        </RouterLink> |
+        <RouterLink to="/">
+          Teletype
+        </RouterLink>
       </div>
       <div class="text-white">
         <RouterLink to="/">
@@ -104,7 +137,6 @@ const routes = {
     </div>
   </section>
 </template>
-
 <style lang="scss">
 .section-screen-content {
   @apply flex flex-col items-center md:flex-row md:justify-around
---
+++ resources/js/src/views/SignupView.vue
@@ -0,0 +1,87 @@
+<template>
+  <LayoutGuest>
+    <SectionFormScreen v-slot="{ cardClass, cardRounded }" bg="signup" promo>
+      <CardForm
+        :class="cardClass"
+        :rounded="cardRounded"
+        form
+        @submit="submit"
+      >
+        <FormField
+          label="Username"
+        >
+          <FormControl
+            v-model="form.login"
+            :icon-right="mdiAccount"
+            name="login"
+            placeholder="johndoe"
+            autocomplete="username"
+          />
+        </FormField>
+
+        <FormField
+          label="Email"
+        >
+          <FormControl
+            v-model="form.email"
+            :icon-right="mdiEmail"
+            name="email"
+            type="email"
+            placeholder="user@example.com"
+            rules="required|email"
+            autocomplete="false"
+          />
+        </FormField>
+
+        <FormField
+          label="Password"
+        >
+          <FormControl
+            v-model="form.pass"
+            type="password"
+            name="password"
+            placeholder="Password"
+            autocomplete="new-password"
+          />
+        </FormField>
+        <template #footer>
+          <BaseLevel mobile>
+            <BaseButton
+              label="Signup"
+              type="submit"
+              color="info"
+            />
+            <RouterLink
+              to="/login"
+              class="text-sm"
+            >
+              Have an account?
+            </RouterLink>
+          </BaseLevel>
+        </template>
+      </CardForm>
+    </SectionFormScreen>
+  </LayoutGuest>
+</template>
+
+<script setup lang="ts">
+import BaseButton from '@/components/template/BaseButton.vue';
+import BaseLevel from '@/components/template/BaseLevel.vue';
+import CardForm from '@/components/template/CardForm.vue';
+import FormControl from '@/components/template/FormControl.vue';
+import FormField from '@/components/template/FormField.vue';
+import SectionFormScreen from '@/components/template/SectionFormScreen.vue';
+import LayoutGuest from '@/layouts/LayoutGuest.vue';
+import { mdiAccount, mdiEmail } from '@mdi/js';
+import { reactive } from 'vue';
+
+const form = reactive({
+  login: 'johndoe',
+  email: 'john.doe@example.com',
+  pass: 'secret'
+})
+
+const submit = () => {
+  console.log('Submit');
+}
+</script>
