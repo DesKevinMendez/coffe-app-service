6365a77ad3475d790d71c15c04b534d32908dd05
Kevin <kevin_mendez30@outlook.com>
2022-11-09 10:01:03 -0600

    feat: 🎸 (Signup) sending data from browsers

--- resources/js/src/views/SignupView.vue
+++ resources/js/src/views/SignupView.vue
@@ -1,27 +1,18 @@
 <template>
   <LayoutGuest>
     <SectionFormScreen v-slot="{ cardClass, cardRounded }" bg="signup" promo>
-      <CardForm
-        :class="cardClass"
-        :rounded="cardRounded"
-        form
-        @submit="submit"
-      >
-        <FormField
-          label="Username"
-        >
+      <CardForm :class="cardClass" :rounded="cardRounded" form @submit="submit">
+        <FormField label="Username">
           <FormControl
-            v-model="form.login"
+            v-model="form.name"
             :icon-right="mdiAccount"
-            name="login"
+            name="name"
             placeholder="johndoe"
             autocomplete="username"
           />
         </FormField>
 
-        <FormField
-          label="Email"
-        >
+        <FormField label="Email">
           <FormControl
             v-model="form.email"
             :icon-right="mdiEmail"
@@ -33,28 +24,30 @@
           />
         </FormField>
 
-        <FormField
-          label="Password"
-        >
+        <FormField label="Password">
           <FormControl
-            v-model="form.pass"
+            v-model="form.password"
             type="password"
             name="password"
             placeholder="Password"
+            rules="required|passwordStrong"
+            autocomplete="new-password"
+          />
+        </FormField>
+        <FormField label="Password confirmation">
+          <FormControl
+            v-model="form.password_confirmation"
+            type="password"
+            name="password_confirmation"
+            placeholder="Password"
+            rules="confirmed:@password"
             autocomplete="new-password"
           />
         </FormField>
         <template #footer>
           <BaseLevel mobile>
-            <BaseButton
-              label="Signup"
-              type="submit"
-              color="info"
-            />
-            <RouterLink
-              to="/login"
-              class="text-sm"
-            >
+            <BaseButton label="Signup" type="submit" color="info" />
+            <RouterLink to="/login" class="text-sm">
               Have an account?
             </RouterLink>
           </BaseLevel>
@@ -71,17 +64,40 @@ import CardForm from '@/components/template/CardForm.vue';
 import FormControl from '@/components/template/FormControl.vue';
 import FormField from '@/components/template/FormField.vue';
 import SectionFormScreen from '@/components/template/SectionFormScreen.vue';
+import useRequest from '@/composables/useRequest';
 import LayoutGuest from '@/layouts/LayoutGuest.vue';
+import { useAuth } from '@/stores/auth';
 import { mdiAccount, mdiEmail } from '@mdi/js';
 import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const request = useRequest();
 
 const form = reactive({
-  login: 'johndoe',
+  name: 'johndoe',
   email: 'john.doe@example.com',
-  pass: 'secret'
-})
+  password: 'Hola123!',
+  password_confirmation: 'Hola123!',
+  device_name: Math.random(),
+});
 
-const submit = () => {
-  console.log('Submit');
-}
+const uRou = useRouter();
+const auth = useAuth();
+
+const submit = async () => {
+  await request.get('sanctum/csrf-cookie');
+  const { data, statusCode } = await request.post<{ token: string }>(
+    'api/register',
+    {
+      ...form,
+    }
+  );
+
+  if (statusCode.value == 422) {
+    return;
+  }
+
+  auth.setLogin(data.value.token);
+  uRou.push({ name: 'home' });
+};
 </script>
