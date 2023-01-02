3ac3985edd343afeb4af32420e6a151a5378da44
Kevin <kevin_mendez30@outlook.com>
2023-01-02 16:08:33 -0600

    refactor: 💡 (model) slug from trait

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,18 +2,17 @@
 
 namespace App\Models;
 
-use App\Traits\{useIsActive, usePaginate};
+use App\Traits\{useIsActive, usePaginate, useSlug};
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Cviebrock\EloquentSluggable\Sluggable;
 
 class Company extends Model
 {
     use HasFactory,
         usePaginate,
         SoftDeletes,
-        Sluggable,
+        useSlug,
         useIsActive;
 
     /**
@@ -22,18 +21,4 @@ class Company extends Model
      * @var array
      */
     protected $guarded = ['id'];
-
-    /**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable(): array
-    {
-        return [
-            'slug' => [
-                'source' => 'name'
-            ]
-        ];
-    }
 }
